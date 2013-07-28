0876d2d382ec44e4fe3b6c8af07b3127f9076102
oscarsan <oscarsanhueza@gmail.com>
2013-07-28 03:35:07 +0300

    take out hardcode token and create one automatically

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,21 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '1b77303f14ca3553beb4e68367bba445c1fa3cb1bd70b8c4c6258ee9f6930fd98f2952a0c4b19eef41b746356c4facecb16673af49da5ffd89b775a5acdeec64'
+#SampleApp::Application.config.secret_token = '1b77303f14ca3553beb4e68367bba445c1fa3cb1bd70b8c4c6258ee9f6930fd98f2952a0c4b19eef41b746356c4facecb16673af49da5ffd89b775a5acdeec64'
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token

cd684fb178750f6d4424eeef161bf62a2587e697
oscarsan <oscarsanhueza@gmail.com>
2013-07-28 18:38:52 +0300

    Add contact static page
    Signed-off-by: oscarsan <oscarsanhueza@gmail.com>

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -59,4 +59,18 @@ describe "Static pages" do
 
   end
 
+  describe "Contact" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the right title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+
+  end
+
 end

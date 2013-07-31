f23003d84901f43b4231f811237173cc380112ec
oscarsan <oscarsanhueza@gmail.com>
2013-07-31 17:50:41 +0300

    Adding User controller functionallity and sign in
    Signed-off-by: oscarsan <oscarsanhueza@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,21 @@ class UsersController < ApplicationController
   attr_accessor :name, :email
   def new
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])    # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Oscar Sanhueza"
+    email    "oscarsanhueza@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end

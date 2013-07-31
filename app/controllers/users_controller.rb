36f87a460f8cea82989e167036f3671d25172575
oscarsan <oscarsanhueza@gmail.com>
2013-07-31 18:49:48 +0300

    finish user signin
    Signed-off-by: oscarsan <oscarsanhueza@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def create
-    @user = User.new(params[:user])    # Not the final implementation!
+    @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-      # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
+   private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end

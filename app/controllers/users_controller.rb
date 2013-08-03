bc56f1af98785529776b4425c85b51febf1dbae6
oscarsan <oscarsanhueza@gmail.com>
2013-08-04 01:35:54 +0300

    Check for right user
    Now the webpage check that right user
    is signed in before going to edit page,
    if wrong user in there it will sigout on
    attempt to edit. Also if try to edit before
    signin it will rememeber after make the singin
    
    Signed-off-by: oscarsan <oscarsanhueza@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   attr_accessor :name, :email
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def new
   end
@@ -44,6 +46,21 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
  
+     # Before filters
 
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      unless current_user?(@user) 
+        sign_out
+        redirect_to(root_url)
+      end
+    end
 
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,15 +1,16 @@
 include ApplicationHelper
 
-def valid_signin(user, Options={})
-if options[:no_capybara]
-    # Sign in when not using Capybara.
-    remember_token = User.new_remember_token
-    cookies[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.encrypt(remember_token))
-else
-  fill_in "Email",    with: user.email
-  fill_in "Password", with: user.password
-  click_button "Sign in"
+def sign_in(user, options={})
+  if options[:no_capybara]
+      # Sign in when not using Capybara.
+      remember_token = User.new_remember_token
+      cookies[:remember_token] = remember_token
+      user.update_attribute(:remember_token, User.encrypt(remember_token))
+  else
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
 end
 
 RSpec::Matchers.define :have_error_message do |message|

75f59d9ac933ab910652ce0b57c570aee906ecd7
oscarsan <oscarsanhueza@gmail.com>
2013-09-21 16:38:40 +0300

    user test for micropost association
    Signed-off-by: oscarsan <oscarsanhueza@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -145,4 +145,18 @@ describe User do
     it { should be_admin }
   end
 
+  #test for micropost association
+  describe "micropost associations" do
+    before { @user.save}
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do 
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right micropost in the right order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end
+  end
 end
